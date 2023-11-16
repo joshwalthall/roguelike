d3f2d64278030831e633a2b37745bcd2efe2d0f0
Josh Walthall <josh.walthall@gmail.com>
2023-11-15 20:17:35 -0500

    Add Actor and simple Player movement

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const gameContainer = document.getElementById('game-container');
 const mapContainer = document.getElementById('map-container');
 
-const tileSize = 32; // Will be converted to pixels
-const mapWidth = 28; // Map width in tiles
-const mapHeight = 18; // Map height in tiles
-
 const blankImgSrc = "assets/img/blank.png";
 const playerImgSrc = "assets/img/player_32px.png";
 const dirtImgSrc = "assets/img/dirt.png";
 
+const moveUp = "w";
+const moveLeft = "a";
+const moveDown = "s";
+const moveRight = "d";
+
 const Map = (function () {
-    const tileCountX = 28;
-    const tileCountY = 18;
+    const tileSize = 32; // Will be converted to pixels
+    const tileCountX = 28; // Map width in tiles
+    const tileCountY = 18; // Map height in tiles
+    const playerStartPos = {X: 11, Y: 6};
     const gridRows = [];
     const dirtChance = 0.07;
 
@@ -23,20 +26,22 @@ const Map = (function () {
         for (y = 1; y <= tileCountY; y++) {
             let gridRow = [];
             for (x = 1; x <= tileCountX; x++) {
-                let gridSquareDiv = document.createElement('div');
-                gridSquareDiv.style.gridArea = `${y} / ${x} / ${y+1} / ${x+1}`;
-                gridSquareDiv.style.width = `${tileSize}px`;
-                gridSquareDiv.style.height = `${tileSize}px`;
-                gridSquareDiv.classList.add('grid-square');
-                mapContainer.appendChild(gridSquareDiv);
-                const gridSquare = {
-                    div: gridSquareDiv,
+                let gridTileDiv = document.createElement('div');
+                gridTileDiv.style.gridArea = `${y} / ${x} / ${y+1} / ${x+1}`;
+                gridTileDiv.style.width = `${tileSize}px`;
+                gridTileDiv.style.height = `${tileSize}px`;
+                gridTileDiv.classList.add('grid-tile');
+                mapContainer.appendChild(gridTileDiv);
+                const gridTile = {
+                    div: gridTileDiv,
+                    img: null,
                     actor: null,
-                    items: [],
                     object: null,
-                    imgSrc: "",
+                    items: [],
+                    baseImgSrc: "",
+                    displayImgSrc: "",
                 };
-                gridRow.push(gridSquare);
+                gridRow.push(gridTile);
             };
             gridRows.push(gridRow);
         };
@@ -45,27 +50,96 @@ const Map = (function () {
         for (y = 0; y < tileCountY; y++) {
             let gridRow = gridRows[y]; // Get row by grid y index
             for (x = 0; x < tileCountX; x++) {
-                let gridSquareDiv = gridRow[x].div; // Get div by row x index
-                let randomNumber = Math.random();
+                let gridTile = gridRow[x]
+                let gridTileDiv = gridTile.div; // Get div by row x index
                 let tileImage = new Image(tileSize, tileSize);
+                gridTile.img = tileImage;
+                let baseImgSrc = "";
+                let randomNumber = Math.random();                
                 if (randomNumber <= dirtChance) {
-                    tileImage.src = dirtImgSrc;
+                    baseImgSrc = dirtImgSrc;
                 } else {
-                    tileImage.src = blankImgSrc;
+                    baseImgSrc = blankImgSrc;
                 };
-                gridSquareDiv.appendChild(tileImage);
+                gridTile.baseImgSrc = baseImgSrc;
+                gridTile.displayImgSrc = gridTile.baseImgSrc;
+                tileImage.src = gridTile.displayImgSrc;
+                gridTileDiv.appendChild(tileImage);
             };
         };
     };
-    const createGrid = () => {
+    const _getTile = (tilePos) => {
+        return gridRows[tilePos.Y][tilePos.X];
+    };
+    const _updateTileImg = (tile, newImgSrc) => {
+        tile.displayImgSrc = newImgSrc;
+        tile.img.src = tile.displayImgSrc;
+    };
+    const updateTileActor = (tilePos, tileActor) => {
+        let tile = _getTile(tilePos);
+        tile.actor = tileActor;
+        let newImgSrc = "";
+        if (tileActor === null) {
+            newImgSrc = tile.baseImgSrc;
+        } else {
+            newImgSrc = tileActor.imgSrc;
+        };
+        _updateTileImg(tile, newImgSrc);
+    };
+    const initializeGrid = () => {
         _setTileCounts();
         _createGrid();
         _populateGrid();
     };
 
     return {
-        createGrid,
+        playerStartPos,
+        initializeGrid,
+        updateTileActor,
     };
 })();
 
-Map.createGrid();
+const Actor = (actorType, actorImgSrc, startPos) => {
+    const type = actorType;
+    const pos = {X: startPos.X, Y: startPos.Y};
+    let imgSrc = actorImgSrc;
+
+    const _setPos = (newPosX, newPosY) => {
+        pos.X = newPosX;
+        pos.Y = newPosY;
+    };
+    const move = (keyDown) => {
+        pressedKey = `${keyDown.key}`.toLowerCase();
+        let oldPos = {X: pos.X, Y: pos.Y};
+        let newPos = {X: pos.X, Y: pos.Y};
+        if (pressedKey === moveUp) {
+            newPos.Y = (pos.Y - 1);
+        } else if (pressedKey === moveLeft) {
+            newPos.X = (pos.X - 1);
+        } else if (pressedKey === moveDown) {
+            newPos.Y = (pos.Y + 1);
+        } else if (pressedKey === moveRight) {
+            newPos.X = (pos.X + 1);
+        } else {
+            console.log(`Invalid key input: "${pressedKey}" not bound to any action`);
+        };
+        _setPos(newPos.X, newPos.Y);
+        Map.updateTileActor(oldPos, null);
+        Map.updateTileActor(newPos, Player);
+    };
+
+    return {
+        type,
+        pos,
+        imgSrc,
+        move,
+    };
+};
+
+Map.initializeGrid();
+
+const Player = Actor("player", playerImgSrc, Map.playerStartPos);
+Map.updateTileActor(Player.pos, Player);
+document.addEventListener("keydown", Player.move)
+
+
